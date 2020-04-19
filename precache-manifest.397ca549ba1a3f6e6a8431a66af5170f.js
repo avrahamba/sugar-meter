33ba9b7cff1dbb3d9eaa8e4f7db683cc806c6c21
Avraham <b.a.avraham@gmail.com>
2020-04-19 21:19:03 +0300

    Initial dist subtree commit

---
+++ precache-manifest.397ca549ba1a3f6e6a8431a66af5170f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "527b9e8475b435b48ba6",
+    "url": "/sugar-meter/css/app.01d544d1.css"
+  },
+  {
+    "revision": "4fd2e60523e6063398198744083eaef8",
+    "url": "/sugar-meter/img/View-Details.4fd2e605.svg"
+  },
+  {
+    "revision": "195d4973f31510db8affe7394efd2097",
+    "url": "/sugar-meter/img/blad-test.195d4973.svg"
+  },
+  {
+    "revision": "3f59e2bc4228e142315673b8f382629f",
+    "url": "/sugar-meter/index.html"
+  },
+  {
+    "revision": "527b9e8475b435b48ba6",
+    "url": "/sugar-meter/js/app.d406cf8b.js"
+  },
+  {
+    "revision": "55574ddc1392c20515de",
+    "url": "/sugar-meter/js/chunk-vendors.193eafcd.js"
+  },
+  {
+    "revision": "d23d27092d8397095fc5a6bc69542ed8",
+    "url": "/sugar-meter/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/sugar-meter/robots.txt"
+  }
+]);
